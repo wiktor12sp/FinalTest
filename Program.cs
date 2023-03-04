0d6795937855f100aacb12f3cda966fb4ab9e4eb
wiktor12sp <120725874+wiktor12sp@users.noreply.github.com>
2023-03-04 11:57:53 +0300

    Add files via upload
    добавил

--- Program.cs
+++ Program.cs
@@ -1,49 +1,13 @@
-﻿строка [] inputArray  = { " привет " , " 2 " , " мир " , " :-) " , " 1234 " , " 1567 " };
+﻿string [] startingArray = new string[] {"hello", "2", "world", ":-)"};
+string[] resultArray = new string [0];
 
-// Метод печати массива
-строка  PrintArray ( строка [] inputArray )
+foreach (var item in startingArray)
 {
-    возвращаемая  строка . Присоединиться ( " , " , inputArray );
-}
-
-// Метод подсчета строк в массиве длиной меньше или равной 3 символам
-int  CountStringInArray ( строка [] ввод )
-{
-    int  len  ​​=  inputArray . длина ;
-     число  счетчиков =  0 ;
-    интервал  maxLengthString  =  3 ;
-    для ( int  i  =  0 ; я  <  len ; я ++ )
+    if (item.Length <= 3)
     {
-        если ( inputArray [ i ]. Length  <=  maxLengthString )
-        {
-            считать ++ ;
-        }
+    Array.Resize(ref resultArray, resultArray.Length + 1);
+    resultArray [resultArray.Length-1] = item;
     }
-     количество возвратов ;
 }
 
-// Метод для создания нового массива из строк, меньших или равных 3 символам, которые он находит во входном массиве
-// ! передать результат метода CountStringInArray в аргумент countWords!
-строка [] CreateNewArray ( строка [] inputArray , int  countWords )
-{
-    int  len  ​​=  inputArray . длина ;
-    строка [] outputArray  =  новая  строка [ countWords ];
-     число  счетчиков =  0 ;
-    интервал  maxLengthString  =  3 ;
-    для ( int  i  =  0 ; я  <  len ; я ++ )
-    {
-        если ( inputArray [ i ]. Length  <=  maxLengthString )
-        {
-            outputArray [ count ] =  inputArray [ i ];
-            считать ++ ;
-        }
-    }
-
-    вернуть  выходной массив ;
-}
-
-Массив печати ( входной массив );
-
-string [] outputArray  =  CreateNewArray ( inputArray , CountStringInArray ( inputArray ));
-
-Массив печати ( выходной массив );
+Console.WriteLine($"[{String.Join("; ", resultArray)}]");
